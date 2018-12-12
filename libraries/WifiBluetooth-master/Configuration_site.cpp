141373c9073bbededa9f15935e78f7e45e8f1918
charleslemaire0 <Charles_Lemaire@gmx.net>
2018-12-12 21:37:48 +0100

    change order of entry to  Lat Long Elevation

--- libraries/WifiBluetooth-master/Configuration_site.cpp
+++ libraries/WifiBluetooth-master/Configuration_site.cpp
@@ -99,22 +99,6 @@ void wifibluetooth::handleConfigurationSite() {
 #ifdef OETHS
       client->print(data); data = "";
 #endif
-      // Longitude
-      if (!sendCommand(":Gg#", temp1)) strcpy(temp1, "+000*00");
-      temp1[4] = 0; // deg. part only
-      data += html_configLongWE1;
-      temp1[0] == '+' ? data += "<option selected value='0'>West</option>" : data += "<option value='0'>West</option>";
-      temp1[0] == '-' ? data += "<option selected value='1'>East</option>" : data += "<option value='1'>East</option>";
-      data += html_configLongWE2;
-      temp1[0] = '0'; // sign
-      sprintf(temp, html_configLongDeg, temp1);
-      data += temp;
-      sprintf(temp, html_configLongMin, (char*)&temp1[5]);
-      data += temp;
-#ifdef OETHS
-      client->print(data); data = "";
-#endif
-
       // Latitude
       if (!sendCommand(":Gt#", temp1)) strcpy(temp1, "+00*00");
       temp1[3] = 0; // deg. part only
@@ -126,6 +110,21 @@ void wifibluetooth::handleConfigurationSite() {
       sprintf(temp, html_configLatDeg, temp1);
       data += temp;
       sprintf(temp, html_configLatMin, (char*)&temp1[4]);
+      data += temp;
+#ifdef OETHS
+      client->print(data); data = "";
+#endif
+      // Longitude
+      if (!sendCommand(":Gg#", temp1)) strcpy(temp1, "+000*00");
+      temp1[4] = 0; // deg. part only
+      data += html_configLongWE1;
+      temp1[0] == '+' ? data += "<option selected value='0'>West</option>" : data += "<option value='0'>West</option>";
+      temp1[0] == '-' ? data += "<option selected value='1'>East</option>" : data += "<option value='1'>East</option>";
+      data += html_configLongWE2;
+      temp1[0] = '0'; // sign
+      sprintf(temp, html_configLongDeg, temp1);
+      data += temp;
+      sprintf(temp, html_configLongMin, (char*)&temp1[5]);
       data += temp;
 #ifdef OETHS
       client->print(data); data = "";
