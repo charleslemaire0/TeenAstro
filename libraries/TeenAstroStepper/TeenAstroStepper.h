b3506459f567c08853909cda96bed4aad9e3c11f
charleslemaire0 <Charles_Lemaire@gmx.net>
2019-05-25 18:41:34 +0200

    Update TeenAstroStepper.h

--- libraries/TeenAstroStepper/TeenAstroStepper.h
+++ libraries/TeenAstroStepper/TeenAstroStepper.h
@@ -167,11 +167,12 @@ public:
       SPI.begin();
       pinMode(MISO, INPUT_PULLUP);
       m_tmc5160->push();
-      m_tmc5160->tbl(0);
-      m_tmc5160->toff(2);
-      m_tmc5160->hstrt(0);
-      m_tmc5160->hend(2);
-      m_tmc5160->intpol(true);
+      m_tmc5160->tbl(2);
+      m_tmc5160->toff(3);
+      m_tmc5160->hstrt(4);
+      m_tmc5160->hend(1);   
+      m_tmc5160->en_pwm_mode(true);
+      m_tmc5160->TPWMTHRS(1000);
       setCurrent(Curr);
       setMicrostep(Micros);
       if (EnPin > 0)
