5a23d5102df26d51334a86415115deb471c7edb0
charleslemaire0 <Charles_Lemaire@gmx.net>
2019-05-26 12:11:47 +0200

    remove old catalog

--- libraries/TeenAstroCatalogOld/src/constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define NUM_HERSCHEL 388
-#define NUM_MESSIER 109
-#define NUM_STARS 408
-#define NUM_NGC 2367
-#define NUM_IC 473
-#define ra_cf 65536.0 / 360.0
-#define de_cf 32767.0 / 90.0
