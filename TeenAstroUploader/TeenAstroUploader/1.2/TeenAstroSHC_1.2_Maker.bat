72f354b4cd678bb4bcf3e2e5a5621eb70ef555bb
charleslemaire0 <Charles_Lemaire@gmx.net>
2020-05-04 20:37:51 +0200

    Update TeenAstroSHC_1.2_Maker.bat

--- TeenAstroUploader/TeenAstroUploader/1.2/TeenAstroSHC_1.2_Maker.bat
+++ TeenAstroUploader/TeenAstroUploader/1.2/TeenAstroSHC_1.2_Maker.bat
@@ -1,7 +1,7 @@
-set OUTPUT=./TeenAstroSHC_beta
-set BOARD=esp8266:esp8266:d1_mini:CpuFrequency=80,VTable=flash,FlashSize=4M1M,LwIPVariant=v2mss536,Debug=Disabled,DebugLevel=None____,FlashErase=none,UploadSpeed=921600
+set OUTPUT=./TeenAstroSHC_1.2
+set BOARD=esp8266:esp8266:d1_mini:xtal=80,vt=flash,eesz=4M1M,ip=lm2n,dbg=Disabled,lvl=None____,wipe=none,baud=921600
 set OPTIONS=--build-properties compiler.cpp.extra_flags=-DLANGUAGE
-set SKETCH=..\..\..\SmartHandController\SmartHandController.ino
-REM ..\..\..\ArduinoCli\arduino-cli.exe compile -v -b %BOARD% -o %OUTPUT%_French %OPTIONS%=FRENCH %SKETCH%
-REM ..\..\..\ArduinoCli\arduino-cli.exe compile -v -b %BOARD% -o %OUTPUT%_German %OPTIONS%=GERMAN %SKETCH%
+set SKETCH=..\..\..\TeenAstroSHC\TeenAstroSHC.ino
+..\..\..\ArduinoCli\arduino-cli.exe compile -v -b %BOARD% -o %OUTPUT%_French %OPTIONS%=FRENCH %SKETCH%
+..\..\..\ArduinoCli\arduino-cli.exe compile -v -b %BOARD% -o %OUTPUT%_German %OPTIONS%=GERMAN %SKETCH%
 ..\..\..\ArduinoCli\arduino-cli.exe compile -v -b %BOARD% -o %OUTPUT%_English %OPTIONS%=ENGLISH %SKETCH%
