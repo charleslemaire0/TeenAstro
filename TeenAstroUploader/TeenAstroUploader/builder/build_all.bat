75256f4d1d364c749d596747328bc2e83a336ce6
charleslemaire0 <Charles_Lemaire@gmx.net>
2020-06-11 14:38:18 +0200

    fix

--- TeenAstroUploader/TeenAstroUploader/builder/build_all.bat
+++ TeenAstroUploader/TeenAstroUploader/builder/build_all.bat
@@ -2,10 +2,7 @@ call TeenAstroBuilder.bat -target MainUnit -driver TMC5160 -hwvers 240 -release 
 call TeenAstroBuilder.bat -target MainUnit -driver TMC2130 -hwvers 240 -release 1.2 -v > log.txt
 call TeenAstroBuilder.bat -target MainUnit -driver TMC260 -hwvers 220 -release 1.2 -v > log.txt
 call TeenAstroBuilder.bat -target MainUnit -driver TMC260 -hwvers 230 -release 1.2 -v > log.txt
-call TeenAstroBuilder.bat -target SHC -local ENGLISH -release 1.2 > log.txt
-call TeenAstroBuilder.bat -target SHC -local FRENCH -release 1.2 > log.txt
-call TeenAstroBuilder.bat -target SHC -local GERMAN -release 1.2 > log.txt
-call TeenAstroBuilder.bat -target SHC -Focuser -driver TMC2130 -hwvers 220 -release 1.2 -v > log.txt
-call TeenAstroBuilder.bat -target SHC -Focuser -driver TMC2130 -hwvers 230 -release 1.2 -v > log.txt
-call TeenAstroBuilder.bat -target SHC -Focuser -driver TMC2130 -hwvers 240 -release 1.2 -v > log.txt
-call TeenAstroBuilder.bat -target SHC -Focuser -driver TMC5160 -hwvers 240 -release 1.2 -v > log.txt
+call TeenAstroBuilder.bat -target Focuser -driver TMC2130 -hwvers 220 -release 1.2 -v > log.txt
+call TeenAstroBuilder.bat -target Focuser -driver TMC2130 -hwvers 230 -release 1.2 -v > log.txt
+call TeenAstroBuilder.bat -target Focuser -driver TMC2130 -hwvers 240 -release 1.2 -v > log.txt
+call TeenAstroBuilder.bat -target Focuser -driver TMC5160 -hwvers 240 -release 1.2 -v > log.txt
