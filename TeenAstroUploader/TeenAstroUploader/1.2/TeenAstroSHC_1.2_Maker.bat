d2ddf6f3ae3b4e324f0781babab8bef6e1f0cdb0
charleslemaire0 <Charles_Lemaire@gmx.net>
2020-06-30 18:49:41 +0200

    update firmware
    Main unit 1.2.1
    SHC 1.2.1

--- TeenAstroUploader/TeenAstroUploader/1.2/TeenAstroSHC_1.2_Maker.bat
+++ TeenAstroUploader/TeenAstroUploader/1.2/TeenAstroSHC_1.2_Maker.bat
@@ -2,7 +2,7 @@ set OUTPUT=./TeenAstroSHC_1.2
 set BOARD=esp8266:esp8266:d1_mini:xtal=80,vt=flash,eesz=4M1M,ip=lm2n,dbg=Disabled,lvl=None____,wipe=none,baud=921600
 set OPTIONS=--build-properties compiler.cpp.extra_flags=-DLANGUAGE
 set SKETCH=..\..\..\TeenAstroSHC\TeenAstroSHC.ino
-..\..\..\Arduino-Cli\arduino-cli.exe config init
-..\..\..\Arduino-Cli\arduino-cli.exe compile --libraries ..\..\..\libraries -v -b %BOARD% -o %OUTPUT%_French %OPTIONS%=FRENCH %SKETCH%
-..\..\..\Arduino-Cli\arduino-cli.exe compile --libraries ..\..\..\libraries -v -b %BOARD% -o %OUTPUT%_German %OPTIONS%=GERMAN %SKETCH%
-..\..\..\Arduino-Cli\arduino-cli.exe compile --libraries ..\..\..\libraries -v -b %BOARD% -o %OUTPUT%_English %OPTIONS%=ENGLISH %SKETCH%
+..\..\..\ArduinoCli\arduino-cli.exe config init
+..\..\..\ArduinoCli\arduino-cli.exe compile --libraries ..\..\..\libraries -v -b %BOARD% -o %OUTPUT%_French %OPTIONS%=FRENCH %SKETCH%
+..\..\..\ArduinoCli\arduino-cli.exe compile --libraries ..\..\..\libraries -v -b %BOARD% -o %OUTPUT%_German %OPTIONS%=GERMAN %SKETCH%
+..\..\..\ArduinoCli\arduino-cli.exe compile --libraries ..\..\..\libraries -v -b %BOARD% -o %OUTPUT%_English %OPTIONS%=ENGLISH %SKETCH%
