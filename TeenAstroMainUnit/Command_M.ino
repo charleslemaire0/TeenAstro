d3acf21ae7d1149a2a82da7608042b0b4d8da038
charleslemaire0 <Charles_Lemaire@gmx.net>
2022-07-15 14:42:04 +0200

    allow pulse guiding even if tracking is off as it is now conform with ASCOM requirement

--- TeenAstroMainUnit/Command_M.ino
+++ TeenAstroMainUnit/Command_M.ino
@@ -47,7 +47,7 @@ void Command_M()
     //  Returns: Nothing
 
     if ((atoi2((char *)&command[3], &i)) &&
-      ((i > 0) && (i <= 16399)) && sideralTracking && !movingTo && lastError == ERR_NONE &&
+      ((i > 0) && (i <= 16399)) && !movingTo && lastError == ERR_NONE &&
         (GuidingState != GuidingRecenter || GuidingState != GuidingST4))
     {
       if ((command[2] == 'e') || (command[2] == 'w'))
