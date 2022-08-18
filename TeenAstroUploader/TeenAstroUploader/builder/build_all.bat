055aa435e87082f4fe3a870a3faacdee4a125894
charleslemaire0 <Charles_Lemaire@gmx.net>
2022-08-18 17:37:28 +0200

    Release firmwara

--- TeenAstroUploader/TeenAstroUploader/builder/build_all.bat
+++ TeenAstroUploader/TeenAstroUploader/builder/build_all.bat
@@ -6,3 +6,4 @@ call TeenAstroBuilder.bat -target Focuser -driver TMC2130 -hwvers 220 -release 1
 call TeenAstroBuilder.bat -target Focuser -driver TMC2130 -hwvers 230 -release 1.3 -v > log_Foc_TMC2130_230.txt
 call TeenAstroBuilder.bat -target Focuser -driver TMC2130 -hwvers 240 -release 1.3 -v > log_Foc_TMC2130_240.txt
 call TeenAstroBuilder.bat -target Focuser -driver TMC5160 -hwvers 240 -release 1.3 -v > log_Foc_TMC5160_240.txt
+call TeenAstroSHC_Maker.bat
