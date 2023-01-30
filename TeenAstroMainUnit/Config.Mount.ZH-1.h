7ed9be6c193feae632c55b800191eb623acd60f4
charleslemaire0 <CharlesLemaire37@gmail.com>
2023-01-30 20:24:41 +0100

    update Config.Mount.ZH-1

--- TeenAstroMainUnit/Config.Mount.ZH-1.h
+++ TeenAstroMainUnit/Config.Mount.ZH-1.h
@@ -6,7 +6,7 @@
 // MOUNT_TYPE_GEM = 1, MOUNT_TYPE_FORK = 2, MOUNT_TYPE_ALTAZM = 3, MOUNT_TYPE_FORK_ALT = 4
 #define D_mountType 4
 
-#define D_motorA1gear 16
+#define D_motorA1gear 1600
 #define D_motorA1stepRot 400
 #define D_motorA1micro 8
 #define D_motorA1reverse 0
@@ -14,8 +14,8 @@
 #define D_motorA1lowCurr 1200
 #define D_motorA1silent 1
 
-#define D_motorA2gear 16
-#define D_motorA2stepRot 200
+#define D_motorA2gear 800
+#define D_motorA2stepRot 400
 #define D_motorA2micro 8
 #define D_motorA2reverse 0
 #define D_motorA2highCurr 1600
