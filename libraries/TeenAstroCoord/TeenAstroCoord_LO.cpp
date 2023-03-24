683df8a525b9091b945aeae088345b042e7d3739
charleslemaire0 <CharlesLemaire37@gmail.com>
2023-03-24 20:19:43 +0100

    Add Local coordinate definition for Spiral Search

---
+++ libraries/TeenAstroCoord/TeenAstroCoord_LO.cpp
@@ -0,0 +1,34 @@
+#include "TeenAstroCoord_EQ.hpp"
+#include "TeenAstroCoord_LO.hpp"
+
+
+Coord_LO::Coord_LO(double Axis3, double Axis2, double Axis1) : Coord({ LA3::RotAxis::ROTAXISX, Axis3 }, { LA3::RotAxis::ROTAXISY, Axis2 }, { LA3::RotAxis::ROTAXISZ, Axis1 })
+{
+};
+Coord_EQ Coord_LO::To_Coord_EQ(const double(&trafo)[3][3])
+{
+  double fre, ha, dec;
+  double tmp1[3][3];
+  double tmp2[3][3];
+  LA3::SingleRotation rots[3] = {
+    m_Eulers[0],
+    m_Eulers[1],
+    m_Eulers[2]
+  };
+  LA3::getMultipleRotationMatrix(tmp1, rots, 3);
+  LA3::multiply(tmp2, tmp1, trafo);
+  LA3::getEulerRxRyRz(tmp2, fre, dec, ha);
+  return Coord_EQ(fre, dec, ha );
+};
+double Coord_LO::Axis3()
+{
+  return m_Eulers[0].angle;
+};
+double Coord_LO::Axis2()
+{
+  return m_Eulers[1].angle;
+};
+double Coord_LO::Axis1()
+{
+  return m_Eulers[2].angle;
+};
---
+++ libraries/TeenAstroCoord/TeenAstroCoord_LO.hpp
@@ -0,0 +1,57 @@
+
+/*
+ * Title       TeenAstro coordinate
+ * by          Charles Lemaire
+ *
+ * Copyright (C) Charles Lemaire
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *
+ * Revision History, see GitHub
+ *
+ *
+ * Lib  Authors: Charles Lemaire
+ *
+ * Description:
+ *
+ * Object oriented coordinate definition
+ *
+ */
+#pragma once
+
+#ifndef __TeenAstroCoord_LO_hpp__
+#define __TeenAstroCoord_LO_hpp__
+
+#ifndef M_PI
+#include <math.h> // for M_PI
+#endif
+
+#include "TeenAstroCoord.hpp"
+
+class Coord_EQ;
+
+class Coord_LO : protected Coord {
+public:
+	Coord_LO(double Axis3, double Axis2, double Axis1);
+	Coord_EQ To_Coord_EQ(const double(&trafo)[3][3]);
+	double Axis3();
+	double Axis2();
+	double Axis1();
+};
+
+
+#endif // __TeenAstroCoord_LO_hpp__
+
