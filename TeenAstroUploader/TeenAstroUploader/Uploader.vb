82981111d8c7f79ccb27bff38b9f6afe5fdc689e
charleslemaire0 <CharlesLemaire37@gmail.com>
2024-01-21 16:18:29 +0100

    release uploader 1.5.0

--- TeenAstroUploader/TeenAstroUploader/Uploader.vb
+++ TeenAstroUploader/TeenAstroUploader/Uploader.vb
@@ -150,44 +150,9 @@ Public Class Uploader
     Dim webAddress As String = "http://" & TextBoxIP.Text & "/update"
     Process.Start(webAddress)
   End Sub
-  Private Sub downloadVersion13x(ByRef n As Integer, ByRef sum As Integer)
-    Dim ver As String = "1.3"
-    Dim gitRootAdress As String = "https://github.com/charleslemaire0/TeenAstro/raw/Release_" + ver + "/TeenAstroUploader/TeenAstroUploader/" + ver + "/"
-    Dim Firmwares As New List(Of String)
-    Firmwares.Add("TeenAstroFocuser_" + ver + "_220_TMC2130.hex")
-    Firmwares.Add("TeenAstroFocuser_" + ver + "_230_TMC2130.hex")
-    Firmwares.Add("TeenAstroFocuser_" + ver + "_240_TMC2130.hex")
-    Firmwares.Add("TeenAstroFocuser_" + ver + "_240_TMC5160.hex")
-    Firmwares.Add("TeenAstroSHC_" + ver + "_English.bin")
-    Firmwares.Add("TeenAstroSHC_" + ver + "_French.bin")
-    Firmwares.Add("TeenAstroSHC_" + ver + "_German.bin")
-    Firmwares.Add("TeenAstro_" + ver + "_220_TMC260.hex")
-    Firmwares.Add("TeenAstro_" + ver + "_230_TMC260.hex")
-    Firmwares.Add("TeenAstro_" + ver + "_240_TMC2130.hex")
-    Firmwares.Add("TeenAstro_" + ver + "_240_TMC5160.hex")
-    Firmwares.Add("TeenAstro_" + ver + "_250_TMC2130.hex")
-    Firmwares.Add("TeenAstro_" + ver + "_250_TMC5160.hex")
-    If Not System.IO.Directory.Exists(".\" + ver) Then
-      System.IO.Directory.CreateDirectory(".\" + ver)
-    End If
-    Using client As New System.Net.WebClient()
-      System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12
-      client.Headers.Add("user-agent", "Anything")
-      For Each firmware In Firmwares
-        Try
-          client.DownloadFile(gitRootAdress + firmware, ".\" + ver + "\" + firmware)
-        Catch ex As Exception
-          MsgBox(ex.Message)
-          Exit For
-        End Try
-        n = n + 1
-      Next
-      client.Dispose()
-    End Using
-    sum += Firmwares.Count.ToString
-  End Sub
-  Private Sub downloadVersion14x(ByRef n As Integer, ByRef sum As Integer, ByVal ext As String)
-    Dim ver As String = "1.4"
+
+  Private Sub downloadVersionx(ByRef n As Integer, ByRef sum As Integer, ByVal ext As String, ByVal ver As String)
+
     Dim verdir As String = ver + ext
     Dim gitRootAdress As String = "https://github.com/charleslemaire0/TeenAstro/raw/Release_" + ver + "/TeenAstroUploader/TeenAstroUploader/" + verdir + "/"
     Dim Firmwares As New List(Of String)
@@ -228,12 +193,13 @@ Public Class Uploader
     Dim n As Integer = 0
     Dim sum As Integer = 0
     sum = n
-    downloadVersion13x(n, sum)
-    downloadVersion14x(n, sum, "")
-    downloadVersion14x(n, sum, "_latest")
+    'downloadVersion13x(n, sum)
+    downloadVersionx(n, sum, "", 1.4)
+    downloadVersionx(n, sum, "_latest", 1.4)
+    downloadVersionx(n, sum, "", 1.5)
+    downloadVersionx(n, sum, "_latest", 1.5)
     MsgBox(n.ToString & "of " & sum.ToString & " successfully downloaded!")
   End Sub
-
 
   Private Sub ComboBoxCOMSHC_Click(sender As Object, e As EventArgs) Handles ComboBoxCOMSHC.Click
     ComboBoxCOMSHC.Items.Clear()
@@ -241,4 +207,5 @@ Public Class Uploader
       ComboBoxCOMSHC.Items.Add(sp)
     Next
   End Sub
+
 End Class
