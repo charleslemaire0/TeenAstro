ac905d56b2ee25382bab3941ad4f80af2b54ec3d
charleslemaire0 <CharlesLemaire37@gmail.com>
2024-01-25 17:57:42 +0100

    Release Main Unit 1.5.1 and SHC 1.5.1

--- TeenAstroMainUnit/FirmwareDef.h
+++ TeenAstroMainUnit/FirmwareDef.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #ifndef VERSION
-#define VERSION 250
+#define VERSION 230
 #endif
 
 // firmware info, these are returned by the ":GV?#" commands
 #define FirmwareDate    __DATE__
-#define FirmwareNumber  "1.5.0"
+#define FirmwareNumber  "1.5.1"
 #define FirmwareName    "TeenAstro"
 #define FirmwareTime    "00:00:00"
 // forces initialialization of a host of settings in XEEPROM.
