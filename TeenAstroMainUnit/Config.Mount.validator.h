6d76729905c37fe8a8425987c091aeb190808a90
LordZurp <lordzurp@gmail.com>
2024-04-30 17:15:05 +0200

    add definitions for RC board v2.6.4

---
+++ TeenAstroMainUnit/Config.Mount.validator.h
@@ -0,0 +1,33 @@
+#pragma once
+//This this the mount template for fix mount definition
+
+#define FirmwareSubName "Validator"
+// https://www.printables.com/fr/model/101944-laser-pointer-robot-remotelly-controlled-arduino-p
+
+
+// MOUNT_TYPE_GEM = 1, MOUNT_TYPE_FORK = 2, MOUNT_TYPE_ALTAZM = 3, MOUNT_TYPE_FORK_ALT = 4
+#define D_mountType 4
+
+#define D_motorA1gear 40       // = physical ratio 
+#define D_motorA1stepRot 20
+#define D_motorA1micro 8        // microstep 0 to 8 : 1, 2, 4 ,8, 16, 32, 64, 128, 256
+#define D_motorA1reverse 1
+#define D_motorA1highCurr 800
+#define D_motorA1lowCurr 400
+#define D_motorA1silent 1
+
+#define D_motorA2gear 30       // = physical ratio
+#define D_motorA2stepRot 20
+#define D_motorA2micro 8        // microstep 0 to 8 : 1, 2, 4 ,8, 16, 32, 64, 128, 256
+#define D_motorA2reverse 1
+#define D_motorA2highCurr 800
+#define D_motorA2lowCurr 400
+#define D_motorA2silent 1
+
+#define D_encoderA1plusePerDegree 400
+#define D_encoderA1reverse 0
+
+#define D_encoderA2plusePerDegree 400
+#define D_encoderA2reverse 0
+
+#define DefaultR4 600
