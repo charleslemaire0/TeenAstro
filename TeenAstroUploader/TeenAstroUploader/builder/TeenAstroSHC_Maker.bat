052072967cfc5820fbd9a77cbbe936542f9a88dc
charleslemaire0 <CharlesLemaire37@gmail.com>
2024-05-12 21:22:20 +0200

    release SHC 1.5.5

--- TeenAstroUploader/TeenAstroUploader/builder/TeenAstroSHC_Maker.bat
+++ TeenAstroUploader/TeenAstroUploader/builder/TeenAstroSHC_Maker.bat
@@ -1,7 +1,7 @@
 set VER=1.5
 set SHCNAME=TeenAstroSHC_%VER%
 set OUTPUT=.\%SHCNAME%
-set BOARD=esp8266:esp8266:d1_mini:xtal=80,vt=flash,eesz=4M1M,ip=lm2n,dbg=Disabled,lvl=None____,wipe=none,baud=921600
+set BOARD=esp8266:esp8266:d1_mini:xtal=160,vt=flash,eesz=4M1M,ip=lm2n,dbg=Disabled,lvl=None____,wipe=none,baud=921600
 set OPTIONS=--build-property compiler.cpp.extra_flags=-DLANGUAGE
 set SKETCH=..\..\..\TeenAstroSHC\TeenAstroSHC.ino
 
